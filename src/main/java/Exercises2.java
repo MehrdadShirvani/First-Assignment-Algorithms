ac9ec7d1552c9010247012bbaa7484f499f2129b
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-02-22 20:42:24 +0330

    Completed the methods, and tested them against the provided test cases

--- src/main/java/Exercises2.java
+++ src/main/java/Exercises2.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class Exercises2 {
+public class Exercises2
+{
 
     /*
     Given an array of integers nums and an integer target, return indices of the two numbers
@@ -13,8 +12,17 @@ public class Exercises2 {
     You can return the answer in any order.
     */
 
-    public int[] twoSum(int[] nums, int target) {
-        // TODO
+    public int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length - 1; i++)
+        {
+            for (int j = i + 1; j < numbers.length; j++)
+            {
+                if(numbers[i] + numbers[j] == target)
+                {
+                    return new int[] {i,j};
+                }
+            }
+        }
         return null;
     }
 
@@ -48,9 +56,62 @@ public class Exercises2 {
     Given a roman numeral, convert it to an integer.
     */
 
-    public int romanToInt(String s) {
-        // TODO
-        return 0;
+    public int calculatePatternValue(String text, String pattern, int value)
+    {
+        int result = 0;
+        while(text.contains(pattern))
+        {
+            result += value;
+            text = text.replaceFirst(pattern, "");
+        }
+
+        return result;
+    }
+
+    public int romanToInt(String text)
+    {
+        int number = 0;
+
+        number += calculatePatternValue(text, "IV", 4);
+        text = text.replace("IV", "");
+
+        number += calculatePatternValue(text, "IX", 9);
+        text = text.replace("IX", "");
+
+        number += calculatePatternValue(text, "XL", 40);
+        text = text.replace("XL", "");
+
+        number += calculatePatternValue(text, "XC", 90);
+        text = text.replace("XC", "");
+
+        number += calculatePatternValue(text, "CD", 400);
+        text = text.replace("CD", "");
+
+        number += calculatePatternValue(text, "CM", 900);
+        text = text.replace("CM", "");
+
+        number += calculatePatternValue(text, "I", 1);
+        text = text.replace("I", "");
+
+        number += calculatePatternValue(text, "V", 5);
+        text = text.replace("V", "");
+
+        number += calculatePatternValue(text, "X", 10);
+        text = text.replace("X", "");
+
+        number += calculatePatternValue(text, "L", 50);
+        text = text.replace("L", "");
+
+        number += calculatePatternValue(text, "C", 100);
+        text = text.replace("C", "");
+
+        number += calculatePatternValue(text, "D", 500);
+        text = text.replace("D", "");
+
+        number += calculatePatternValue(text, "M", 1000);
+        text = text.replace("M", "");
+
+        return number;
     }
 
     /*
@@ -58,12 +119,39 @@ public class Exercises2 {
     You can return the answer in any order.
     */
 
-    public List<List<Integer>> permute(int[] nums) {
-        // TODO
-        return null;
+
+
+    List<List<Integer>> result = new ArrayList<>();
+    public void GetOne(List<Integer> numberPools, List<Integer> theCurrentList)
+    {
+        if(numberPools.size() == 0)
+        {
+            result.add(theCurrentList);
+        }
+        for (int i = 0; i < numberPools.size(); i++)
+        {
+            ArrayList<Integer> newCurrentList = new ArrayList<>(theCurrentList);
+            newCurrentList.add(numberPools.get(i));
+            ArrayList<Integer> newNumberPool = new ArrayList<>(numberPools);
+            newNumberPool.remove(i);
+            GetOne(newNumberPool, newCurrentList);
+        }
     }
 
-    public static void main(String[] args) {
+    public List<List<Integer>> permute(int[] nums)
+    {
+        result = new ArrayList<>(); // Very Important, since it's global, it will have the ones from prior tests
+        List<Integer> baseList = new ArrayList<>();
+        for (int num : nums)
+        {
+            baseList.add(num);
+        }
+        GetOne(baseList, new ArrayList<Integer>());
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
         // test your code here!
     }
 }
